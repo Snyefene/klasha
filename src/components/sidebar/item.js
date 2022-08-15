5c1cfea85ee30a9260f71973bf9016b6b1721563
Solomon <solomon@migmentinfo.com>
2022-08-15 19:12:35 +0000

    Add icons to sidebar list

--- src/components/sidebar/item.js
+++ src/components/sidebar/item.js
@@ -1,11 +1,35 @@
 import { Link } from "react-router-dom";
+import {
+    SwapOutlined,
+    SoundOutlined,
+    SyncOutlined,
+    PieChartOutlined,
+    DisconnectOutlined,
+    ShoppingCartOutlined 
+
+    } 
+ from '@ant-design/icons'
+
 const Item = ({item, active, onClick}) => {
     const activeClass = active === item && 'active-item';
     const link = item === 'Dashboard'? '/' : `${item}`
 
+    const getIcon = () => {
+        if(item === 'Dashboard'){return <PieChartOutlined/>};
+        if(item === 'Balances'){return <SoundOutlined />};
+        if(item === 'Transactions'){return <SoundOutlined />};
+        if(item === 'Analytics'){return <SoundOutlined />};
+        if(item === 'Marketing'){return <SoundOutlined />};
+        if(item === 'KlashaCheckout'){return < ShoppingCartOutlined  />}
+        if(item === 'Payment Links'){return <SoundOutlined />}
+        if(item === 'KlashaWire'){return <SoundOutlined />}
+        if(item === 'Exchange rates'){return <SyncOutlined />}
+    }
+
     return (
         <Link to={link}>
         <div className={`item ${activeClass}`} onClick={()=> onClick()}>
+           {getIcon()}&nbsp;&nbsp;
            {item}
         </div>
         </Link>
--- src/lists/sidebar_items.js
+++ src/lists/sidebar_items.js
@@ -1,5 +1,5 @@
 const  sidebar_items = {
-    main_pages: ['Dashboard', 'Balances', 'Transactions', 'Analytics','Marketing'],
+    main_pages: ['Dashboard', 'Balances', 'Transactions', 'Analytics','Marketing', 'Exchange rates'],
     accept_payments: ['KlashaCheckout', 'Payment Links'],
     send_payments: ['KlashaWire'],
 }
